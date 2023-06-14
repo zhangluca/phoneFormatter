171a22dfb6979193c0cd7b6d55e656b0caf78b34
zhangluca <youcandoitttttt@gmail.com>
2023-06-14 12:40:22 -0400

    e

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
+const form = document.getElementById('myForm');
+const username = document.getElementById('name');
+const email = document.getElementById('email');
+const result= document.getElementById('result');
 
-const input=document.getElementById('phone');
-const label=document.getElementById('label')
-const fun1=()=>{
-    if(input.value.length==1){
-        input.value="("+input.value;
-    }
-    if(input.value.length==4){
-        input.value=input.value+")"
-    }
- if(input.value.length==8){
-    input.value=input.value+"-"
+form.addEventListener('submit', async (event) => {
+  event.preventDefault(); // Prevent form submission
 
- }
-}
-input.addEventListener('input', fun1)
+  const url = "https://7bah5wp2f8.execute-api.us-west-2.amazonaws.com/dev";
+  const options = {
+    method: "POST",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+    body: JSON.stringify({ 'name': username.value, 'email': email.value }),
+  };
+
+
+
+  try {
+    const res = await fetch(url, options);
+    const data = await res.json();
+    result.innerText=data.body.slice(1,data.body.length-1);
+    console.log(data)
+  } catch (error) {
+    console.error(error);
+  }
+});
